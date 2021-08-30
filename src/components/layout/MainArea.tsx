22cf3cc340f9edd1feb3a6831051f0372e1fa7c8
zak <codedpills@gmail.com>
2021-08-30 23:32:11 +0000

    render alphabet sorted list

--- src/components/layout/MainArea.tsx
+++ src/components/layout/MainArea.tsx
@@ -1,10 +1,21 @@
 import Titlebar from "./Titlebar";
 import TidyCollectionList from "../tidy-collection/TidyCollectionList";
+import LetterLink from "../letterlink/LetterLink";
 
 const MainArea = () => {
   return (
     <main className="main-area">
-      <Titlebar title="Archive">Alphabet sorting thing</Titlebar>
+      <Titlebar title="Archive">
+        <div className="col-11">
+          <div className="alphabet-sort">
+            <ul className="alphabet-sort__list">
+              {"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter, idx) => (
+                <LetterLink key={idx} letter={letter} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Titlebar>
       <div className="main-area__content">
         <div className="col-11">
           <TidyCollectionList />
