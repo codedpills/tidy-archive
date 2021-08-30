98f1bc5d607c7d212f020a3cf3b89cce3f6bbebe
zak <codedpills@gmail.com>
2021-08-30 20:56:13 +0000

    add main content section

--- src/components/layout/MainArea.tsx
+++ src/components/layout/MainArea.tsx
@@ -1,16 +1,17 @@
-import Titlebar from "./Titlebar"
+import Titlebar from "./Titlebar";
+import TidyCollectionList from "../tidy-collection/TidyCollectionList";
 
 const MainArea = () => {
-    return (
-        <main className="main-area">
-            <Titlebar title="Archive">
-                Alphabet sorting thing
-            </Titlebar>
-            <div className="main-area__content">
-                main content area
-            </div>
-        </main>
-    );
-}
+  return (
+    <main className="main-area">
+      <Titlebar title="Archive">Alphabet sorting thing</Titlebar>
+      <div className="main-area__content">
+        <div className="col-11">
+          <TidyCollectionList />
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default MainArea;
