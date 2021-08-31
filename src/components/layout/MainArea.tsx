6bb1f3f0e1f9d1a2c150f347822910c22a6390f6
zak <codedpills@gmail.com>
2021-08-31 18:31:49 +0000

    set col sizes, display dummy tidy collection list

--- src/components/layout/MainArea.tsx
+++ src/components/layout/MainArea.tsx
@@ -4,7 +4,7 @@ import LetterLink from "../letterlink/LetterLink";
 
 const MainArea = () => {
   return (
-    <main className="main-area">
+    <main className="main-area col-md-9 ms-sm-auto col-lg-10 ">
       <Titlebar title="Archive">
         <div className="col-11">
           <div className="alphabet-sort">
@@ -18,6 +18,9 @@ const MainArea = () => {
       </Titlebar>
       <div className="main-area__content">
         <div className="col-11">
+          <TidyCollectionList />
+          <TidyCollectionList />
+          <TidyCollectionList />
           <TidyCollectionList />
         </div>
       </div>
