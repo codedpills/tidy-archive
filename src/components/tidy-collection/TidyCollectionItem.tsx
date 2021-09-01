d6fd74176afa06de5250adc386913f2ad2e54f30
zak <codedpills@gmail.com>
2021-09-01 16:59:32 +0000

    display archive button on hover

--- src/components/tidy-collection/TidyCollectionItem.tsx
+++ src/components/tidy-collection/TidyCollectionItem.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import ArchiveButton from "../archive-button/ArchiveButton";
+
 type TidyCollectionItemProps = {
   collectionTitle: string;
   collectionIconName: string;
@@ -7,21 +10,44 @@ const TidyCollectionItem = ({
   collectionTitle,
   collectionIconName,
 }: TidyCollectionItemProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const someRestoreFunction = () => {
+    console.log("restore archived collection!");
+  };
+
   return (
-    <div className="tidy-collection-item">
-      <div className="card shadow-sm rounded border-0 py-4 px-4">
-        <div className="row">
-          <div className="col-3">
-            <span className={`collection-icon ${collectionIconName}`}></span>
-          </div>
-          <div className="col-7">
-            <span className="text-uppercase collection-tag">
-              Tidy collection
-            </span>
-            <span className="text-capitalize collection-title">
-              {collectionTitle}
-            </span>
-          </div>
+    <div
+      className="tidy-collection-item"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className={`card shadow-sm rounded border-0 py-4 px-4 ${
+          isHovered && "hover"
+        }`}
+      >
+        <div className="card__icon">
+          <span className={`collection-icon ${collectionIconName}`}></span>
+        </div>
+        <div className="card__text">
+          <span className="text-uppercase collection-tag">Tidy collection</span>
+          <span className="text-capitalize collection-title">
+            {collectionTitle}
+          </span>
+        </div>
+        <div className={`card__button ${isHovered && "visible"}`}>
+          <ArchiveButton
+            isArchived={true}
+            handleArchiveButtonAction={someRestoreFunction}
+          />
         </div>
       </div>
     </div>
