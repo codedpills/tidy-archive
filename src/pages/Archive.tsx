d61b3600640eab7f4fba485f96014a86c8660b94
zak <codedpills@gmail.com>
2021-09-02 20:15:31 +0000

    persist archived & restored frontpages w/ backend

--- src/pages/Archive.tsx
+++ src/pages/Archive.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
 import PageContainer from "../components/layout/PageContainer";
 import MainArea from "../components/layout/MainArea";
@@ -6,109 +6,91 @@ import Sidebar from "../components/layout/Sidebar";
 
 import { Frontpage } from "../types/frontpage";
 
-const sampleFrontpages = [
-  { id: "1", title: "Folder 1", icon: "bi-folder" },
-  { id: "2", title: "Folder 2", icon: "bi-folder-x" },
-  { id: "3", title: "Cash 1", icon: "bi-cash" },
-  { id: "4", title: "Calender 2", icon: "bi-calendar2" },
-  { id: "5", title: "Cloud 1", icon: "bi-cloud-minus" },
-  { id: "6", title: "Emoji 5", icon: "bi-emoji-smile" },
-];
-const sampleArchived = [
-  { id: "7", title: "App indicator", icon: "bi-app-indicator" },
-  { id: "8", title: "Award 1", icon: "bi-award" },
-  { id: "9", title: "Broadcast 1", icon: "bi-broadcast" },
-  { id: "10", title: "Image 1", icon: "bi-image" },
-  { id: "11", title: "Subtract 1", icon: "bi-subtract" },
-  { id: "12", title: "Wallet 2", icon: "bi-wallet2" },
-  { id: "13", title: "Telephone", icon: "bi-telephone" },
-];
-
-type ArchiveState = {
-  frontpages: Frontpage[];
-  archived: Frontpage[][];
-};
 
 const Archive = () => {
-  const initialState: ArchiveState = {
-    frontpages: [],
-    archived: Array(26)
-      .fill(0)
-      .map((i) => []),
-  };
 
-  const [state, setState] = useState<ArchiveState>(initialState);
+  const GET_FRONTPAGES_AND_ARCHIVED = gql`
+    query getFrontpagesAndArchived {
+      frontpages {
+        id
+        title
+        icon
+      }
+      archived {
+        id
+        title
+        icon
+      }
+    }
+  `;
 
-  useEffect(() => {
-    const sortedArchived = Array(26)
-      .fill(0)
-      .map((i) => new Array<Frontpage>());
-    sampleArchived.forEach((archivedFrontpage: Frontpage) => {
-      const index = archivedFrontpage.title.toLowerCase().charCodeAt(0) - 97;
-      sortedArchived[index].push(archivedFrontpage);
+  const ARCHIVE_FRONTPAGE = gql`
+    mutation ArchiveMutation($archiveId: String!) {
+      archive(id: $archiveId)
+    }
+  `;
+
+  const RESTORE_FRONTPAGE = gql`
+    mutation RestoreMutation($restoreId: String!) {
+      restore(id: $restoreId)
+    }
+  `;
+
+  const {
+    loading: fetchLoading,
+    error: fetchError,
+    data: fetchData,
+  } = useQuery(GET_FRONTPAGES_AND_ARCHIVED);
+
+  const archived = fetchData?.archived || [];
+  const frontpages = fetchData?.frontpages || [];
+
+  const [archiveFP] =
+    useMutation(ARCHIVE_FRONTPAGE, {
+      refetchQueries: [
+        GET_FRONTPAGES_AND_ARCHIVED, 
+        'getFrontpagesAndArchived' 
+      ],
     });
-    setState({
-      ...state,
-      archived: sortedArchived,
-      frontpages: sampleFrontpages,
+
+  const [restoreFP] =
+    useMutation(RESTORE_FRONTPAGE, {
+      refetchQueries: [
+        GET_FRONTPAGES_AND_ARCHIVED, 
+        'getFrontpagesAndArchived' 
+      ],
     });
-  }, []);
+
 
   const handleArchive = (id: string) => {
-    const archivedFrontpage = state.frontpages.find(
-      (frontpage) => frontpage.id === id
-    );
-    const updatedFrontpages = state.frontpages.filter(
-      (frontpage) => frontpage.id !== id
-    );
-    if (!archivedFrontpage) {
-      return;
-    }
-    const archivedFrontpageIndex =
-      archivedFrontpage.title.toLowerCase().charCodeAt(0) - 97;
-    const updatedArchived = state.archived.map((archived, idx) => {
-      if (idx === archivedFrontpageIndex) {
-        return [...archived, archivedFrontpage];
-      }
-      return archived;
-    });
-
-    setState({
-      ...state,
-      archived: updatedArchived,
-      frontpages: updatedFrontpages,
-    });
+    archiveFP({ variables: { archiveId: id } });
   };
 
   const handleRestore = (id: string, collectionLetter: string) => {
-    const index = collectionLetter.toLowerCase().charCodeAt(0) - 97;
-    const archivedFrontpage = state.archived[index].find(
-      (archived) => archived.id === id
-    );
-    if (!archivedFrontpage) return;
-    const updatedArchived = state.archived[index].filter(
-      (archived) => archived.id !== id
-    );
-    const updatedFrontpages = [...state.frontpages, archivedFrontpage];
-    setState({
-      ...state,
-      archived: [
-        ...state.archived.slice(0, index),
-        updatedArchived,
-        ...state.archived.slice(index + 1),
-      ],
-      frontpages: updatedFrontpages,
-    });
+    restoreFP({ variables: { restoreId: id } });
   };
 
+  if (fetchLoading) return <p>Loading...</p>;
+  if (fetchError) return <p>Oops! something went wrong. ${fetchError}</p>;
+
+  const sortedArchived = Array(26)
+    .fill(0)
+    .map((i) => new Array<Frontpage>());
+
+  archived.forEach((archivedFrontpage: Frontpage) => {
+    const index = archivedFrontpage.title.toLowerCase().charCodeAt(0) - 97;
+    sortedArchived[index].push(archivedFrontpage);
+  });
+
+  
   return (
     <PageContainer
       side={
-        <Sidebar frontpages={state.frontpages} handleArchive={handleArchive} />
+        <Sidebar frontpages={frontpages} handleArchive={handleArchive} />
       }
       main={
         <MainArea
-          archivedFrontpages={state.archived}
+          archivedFrontpages={sortedArchived}
           handleRestore={handleRestore}
         />
       }
